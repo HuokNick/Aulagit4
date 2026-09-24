b5a1fad69d939ed1e77b784fc7aaae257fc5ba58
Nicolas <nicolas.carvalho755@gmail.com>
2026-09-24 17:46:10 -0300

    chore: Estrutura Inicial do Projeto

---
+++ Aulagit4/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Aulagit4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Aulagit4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
